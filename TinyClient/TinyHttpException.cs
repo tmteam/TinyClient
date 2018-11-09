ab2e1a588ee7706da1ec7b8e3e6670faeed32d4d
tmteam <tmteam@yandex.ru>
2018-11-09 21:37:37 +0300

    Add status code to tiny http exception

--- TinyClient/TinyHttpException.cs
+++ TinyClient/TinyHttpException.cs
@@ -9,28 +9,32 @@ namespace TinyClient
         public IHttpResponse HttpResponse { get; }
 
         public TinyHttpException(IHttpResponse response, Exception innerException) : base(
-            "Http request failed. Response status: " + response.StatusCode, innerException) {
-            this.HttpResponse = response;
+            CreateErrorMessage(response.StatusCode), innerException) {
+            HttpResponse = response;
         }
+        
+        public TinyHttpException(
+            IHttpResponse response, 
+            string message, 
+            Exception innerException, 
+            WebResponse webResponse) 
+            :base(message, innerException, WebExceptionStatus.ProtocolError, webResponse) 
+            => HttpResponse = response;
 
-        public TinyHttpException(IHttpResponse response, string message, Exception innerException, WebResponse webResponse) :base(message, innerException, WebExceptionStatus.ProtocolError, webResponse)
-        {
-            this.HttpResponse = response;
-        } 
-        public TinyHttpException(IHttpResponse response, string message, Exception innerException) : base(
-            message, innerException)
-        {
-            this.HttpResponse = response;
-        }
+        public TinyHttpException(IHttpResponse response, string message, Exception innerException) 
+            : base(message, innerException) 
+            => HttpResponse = response;
 
         public TinyHttpException(IHttpResponse response, string message)
-            : base(message)
-        {
-            this.HttpResponse = response;
-        }
-        public TinyHttpException(IHttpResponse response):base("Http request failed. Response status: "+ response.StatusCode)
-        {
-            this.HttpResponse = response;
-        }
+            : base(message) 
+            => HttpResponse = response;
+
+        public TinyHttpException(IHttpResponse response)
+            :base(CreateErrorMessage(response.StatusCode)) 
+            => HttpResponse = response;
+
+        private static string CreateErrorMessage(HttpStatusCode statusCode) 
+            => $"Http request failed. Response status: {(int)statusCode} {statusCode}";
+
     }
 }
